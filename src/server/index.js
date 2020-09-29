72c21aebf083915a728ff6a2f574f2b9485161b6
Heber Quequejana <heberqc@gmail.com>
2020-09-29 07:28:02 -0500

    load data from server

--- src/server/index.js
+++ src/server/index.js
@@ -4,27 +4,27 @@ import axios from 'axios';
 import React from 'react';
 import ReactDom from 'react-dom/server';
 
-import App from '../client/components/app';
+import App from '../client/components/App';
 
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, '..', '..', 'dist', 'static')));
 
 app.get('/ssr', async (req, res) => {
-    // const response = await axios('http://localhost:4000/items');
-    // const items = response.data;
+    const response = await axios('https://jsonplaceholder.typicode.com/todos');
+    const items = response.data.map(item => ({id: item.id, label: item.title}));
     const root = (
         <html>
             <body>
                 <div id="root">
-                    <App />
+                    <App items={items} />
                 </div>
 
-                {/* <script
+                <script
                     dangerouslySetInnerHTML={{
                         __html: `window.__data__ = ${JSON.stringify(items)};`
                     }}
-                /> */}
+                />
                 <script src="/static/bundle.js"></script>
             </body>
         </html>
